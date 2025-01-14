6612f26fcc129de992c6d998d7c5919785df56cc
agnihotriamey19 <65012502+agnihotriamey19@users.noreply.github.com>
2025-01-14 14:47:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TranslatorApp from "./components/TranslatorApp";
+
+function App() {
+  return (
+    <div className="App">
+      <TranslatorApp />
+    </div>
+  );
+}
+
+export default App;
